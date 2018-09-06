5b8f8ad55178fb051eae0e636535ba47dcc5b26d
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 15:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const driver = {}
   }
 
   function deleteFromDriverByKey(driver, key) {
-    const clone = {...driver}
+    const clone = Object.assign({}, driver)
     delete clone.key
     return clone
   }
